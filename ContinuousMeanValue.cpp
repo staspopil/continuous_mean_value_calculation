f29911a04ab8bda64282b20e8931f223bafcaf25
staspopil <stas1999lp@gmail.com>
2024-02-20 02:00:38 +0300

    first commit

---
+++ ContinuousMeanValue.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter Value, or x to stop!\n";
+
+    float counter = 0, sum = 0;
+    std::string number;
+    std::cin >> number;
+
+    while (number != "x")
+    {
+        counter++;  
+        sum += std::stof(number);
+        std::cout << "mean value: " << sum / counter << "\n";
+        std::cin >> number;
+    }
+    std::cout << "Bye Bye !";
+}
